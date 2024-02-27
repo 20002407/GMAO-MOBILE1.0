f981e65f5aac99c5d98bd10ac9fdf129535f7f71
20002407 <95947177+20002407@users.noreply.github.com>
2024-02-27 15:13:22 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -7,9 +8,29 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private router: Router) { }
 
   navigateToPage(page: string) {
-    this.navCtrl.navigateForward('/' + page);
+    this.navCtrl.navigateForward('/' + page); 
   }
+
+  showDropdown1: boolean = false;
+  showDropdown2: boolean = false;
+  
+  
+
+  toggleDropdown(dropdown: string) {
+    if (dropdown === 'dropdown1') {
+      this.showDropdown1 = !this.showDropdown1;
+      this.showDropdown2 = false; // Close other dropdowns if opened
+    } else if (dropdown === 'dropdown2') {
+      this.showDropdown2 = !this.showDropdown2;
+      this.showDropdown1 = false; // Close other dropdowns if opened
+    } 
+  }
+
+  navigateTo(route: string) {
+    this.router.navigateByUrl(route);
+  }
+
 }
